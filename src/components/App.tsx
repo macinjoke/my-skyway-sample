5063c252ba8d69ccb849f88d517e7c178c1b4591
macinjoke <shunji.makino@gmail.com>
2019-07-28 00:41:16 +0900

    mediaDevice たちをセレクトボックスで表示

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 import Step1 from './Step1'
 import Step2 from './Step2'
 import Step3 from './Step3'
 
+type State = {
+  mediaDevices: MediaDeviceInfo[]
+}
+
+const initialState: State = {
+  mediaDevices: [],
+}
+
+type Action = {
+  type: 'addMediaDevicesInfo'
+  payload: MediaDeviceInfo[]
+}
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'addMediaDevicesInfo':
+      return { mediaDevices: action.payload }
+    default:
+      return state
+  }
+}
+
 const App: React.FC = () => {
   const [step, setStep] = useState(1)
+  const localVideoRef = useRef(null)
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(deviceInfos => {
+      console.log(deviceInfos)
+      dispatch({ type: 'addMediaDevicesInfo', payload: deviceInfos })
+    })
+  }, [])
 
   return (
     <div className="pure-g">
       <div className="pure-u-2-3">
-        <video autoPlay></video>
+        <video ref={localVideoRef} autoPlay></video>
         <video muted autoPlay></video>
       </div>
 
@@ -18,12 +50,28 @@ const App: React.FC = () => {
 
         <div className="select">
           <label htmlFor="audioSource">Audio input source: </label>
-          <select id="audioSource"></select>
+          <select id="audioSource">
+            {state.mediaDevices
+              .filter(device => device.kind === 'audioinput')
+              .map((device, i) => (
+                <option key={device.deviceId}>
+                  {device.label || `Microphone ${i + 1}`}
+                </option>
+              ))}
+          </select>
         </div>
 
         <div className="select">
           <label htmlFor="videoSource">Video source: </label>
-          <select id="videoSource"></select>
+          <select id="videoSource">
+            {state.mediaDevices
+              .filter(device => device.kind === 'videoinput')
+              .map((device, i) => (
+                <option key={device.deviceId}>
+                  {device.label || `Camera ${i + 1}`}
+                </option>
+              ))}
+          </select>
         </div>
         {step === 1 && <Step1 />}
         {step === 2 && <Step2 />}
