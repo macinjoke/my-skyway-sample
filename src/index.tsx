06c19529d5800b02d2bd673385bc74f7843901fd
macinjoke <shunji.makino@gmail.com>
2019-08-04 00:27:20 +0900

    update styled-components to beta

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
-console.log('hoge')
 const element = document.getElementById('app')
 
 render(<App />, element)
